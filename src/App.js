05915ae6e803d7d50981a26721f7e251de7eb437
harshdeep-gill <harshdeep.gill@travelopia.com>
2026-04-02 03:46:08 +0530

    Optimised the code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
-import { useDispatch } from "react-redux";
-import HeaderImage from "./Conponents/HeaderImage";
-import Home from "./Conponents/Home";
-import Navbar from "./Conponents/Navbar";
-import "./Sass/main.scss"
-import About from "./Conponents/About";
-import Skills from "./Conponents/Skills";
-import Projects from "./Conponents/Projects";
-import Github from "./Conponents/Github";
-import Contact from "./Conponents/Contact";
+import Navbar from './components/layout/Navbar';
+import HeaderImage from './components/sections/HeaderImage';
+import Home from './components/sections/Home';
+import About from './components/sections/About';
+import Skills from './components/sections/Skills';
+import Projects from './components/sections/Projects';
+import Github from './components/sections/Github';
+import Contact from './components/sections/Contact';
+import './Sass/main.scss';
 
 function App() {
-
-  const dispatch = useDispatch();
-
-  const handleScroll = () =>{
-    let htmlEl = document.documentElement;
-    let scrollFr = htmlEl.scrollTop / htmlEl.clientHeight;
-    dispatch({type: "SCROLL_CHANGE", payload: scrollFr})
-  }
-
-  window.addEventListener("scroll", handleScroll);
-
-
-  return (
-    <div className="App">
-      <Navbar/>
-      <HeaderImage/>
-      <Home/>
-      <About/>
-      <Skills/>
-      <Projects/>
-      <Github/>
-      <Contact/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <HeaderImage />
+            <Home />
+            <About />
+            <Skills />
+            <Projects />
+            <Github />
+            <Contact />
+        </div>
+    );
 }
 
 export default App;
